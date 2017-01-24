3d042b465256b51b19a6d3d0f77d87ed340e2418
Helana <helanan@gmail.com>
2017-01-23 20:13:11 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+var sonnet = document.getElementById("sonnet")
+var sonnetDiv = document.getElementById("sonnet").innerHTML;
+//console.log("original sonnet: ",sonnetDiv);//
+
+var startingPosition = sonnetDiv.indexOf("orphans");
+console.log("starting position of orphans", startingPosition);
+
+
+
+//switch (startingPosition) {
+	case 410 :
+	case 411 :
+	case 422 :
+	//console.log("hey, you were right.  that never happens");
+	break;
+	case 421 :
+	//console.log("hey, you were wrong.  that never happens");
+	default :
+		//console.log("WTF");
+//}
+
+//console.log("number of characters in sonnet: ", sonnetDiv.length)
+
+//var newContent = sonnetDiv.replace("winter","yuletide").replace(/ the /gi,"a large"); 
+//console.log("both things replaced: ", newContent);
+
+//document.getElementById("sonnet").innerHTML = newContent
+//}
+
+
+var myArray = ["first element",2,false,"last element"];
+
+//for (var i = 0; i < myArray.length; i++) {
+	myArray[i]
+}
+
+for (var i = 0; i < 150; i += 10)
+console.log("current value :", i);
+}
+
+
+
+//var today = new Date();
+//var hourNow = today.getHours();
+//var greeting;
+
+//if (hourNow > 18) {
+    greeting = 'Good evening!';
+//} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+//} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+//} else {
+    greeting = 'Welcome!';
+//}
+
+//document.write('<h3>' + greeting + '</h3>');//
